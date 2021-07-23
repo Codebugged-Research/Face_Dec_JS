6b30fcf21b0eabbe8133bdab6a225555ee4c4a6e
Sayan Nath <sayannath235@gmail.com>
2021-07-24 01:47:45 +0530

    SSL setup done

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const port = process.env.PORT || 5000;
+const cors = require("cors");
+const PORT = 5000;
 const path = require("path");
+const http = require("http");
+const https = require("https");
 
+httpapp.get("*", function (req, res, next) {
+  res.redirect("https://" + req.headers.host + req.path);
+});
 
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", express.static(path.join(__dirname, "/public")));
 
+const httpServer = http.createServer(httpapp);
+httpServer.listen(PORT, () => {
+  console.log("HTTP Server running on port PORT");
+});
+const httpsServer = https.createServer(
+  {
+    key: fs.readFileSync("/etc/letsencrypt/live/codebugged.com/privkey.pem"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/codebugged.com/cert.pem"),
+  },
+  app
+);
 
-app.listen(port, () => {
-  console.log(`server running ${port}`);
+httpsServer.listen(PORT, () => {
+  console.log("HTTPS Server running on port PORT");
 });
